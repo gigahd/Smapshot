27848b1f7c2bcaa9e860d1d3d0fdde91c9371e54
GigaHD <benaltmann2004@gmail.com>
2026-05-26 18:07:10 +0200

    Built and published by Moonwave

---
+++ assets/js/5c271dfa.c1341a8e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[522],{7676:s=>{s.exports=JSON.parse('[{"type":"link","href":"/api/Smapshot","label":"\u200bSmapshot"}]')}}]);
---
+++ assets/js/a17874e2.50c927e3.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[336],{4410:s=>{s.exports=JSON.parse('{"sourceUrl":"https://github.com/gigahd/Smapshot/blob/master","baseUrl":"/Smapshot/","classOrder":[],"apiCategories":[]}')}}]);
